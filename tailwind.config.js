5dc5953f9556a5294b8b3d1ee1ddbebbc9be01c4
Mvshyam <jacobjabakumar2002@gmail.com>
2023-01-19 20:14:12 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["assets"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-  container: {
-    padding: {
-      DEFAULT: '1rem',
-      sm: '2rem',
-      lg: '4rem',
-      xl: '5rem',
-      '2xl': '6rem',
-    },
-  },
 }
